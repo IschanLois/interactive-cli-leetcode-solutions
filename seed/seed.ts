63d0c5a88d442370eb69c176a939343c3ff1105f
Christian Olo <christianlois.olo@gmail.com>
2024-07-23 13:03:24 +0800

    refactor: change metadata format and seed implementation

--- seed/seed.ts
+++ seed/seed.ts
@@ -2,34 +2,33 @@ import { execSync } from 'child_process'
 import { readdir, readFile, writeFile } from 'fs/promises'
 import { join } from 'path'
 
+type Categories = Record<string, string[]>
+
 export default async (): Promise<void> => {
   try {
     const rootDir: string = execSync('git rev-parse --show-toplevel', { encoding: 'utf8' }).trim()
     const metadataDir = join(rootDir, '/assets/metadata')
 
     const directory: string[] = await readdir(metadataDir, { encoding: 'utf8' })
-    const contents: Record<string, string>[] = await Promise.all(
-      directory
-        .map((file: string): Promise<Record<string, string>> => new Promise((resolve) => {
-          readFile(join(metadataDir, file), { encoding: 'utf8' })
-            .then((content: string) => { resolve({ [`${file.split('.')[0]}.cpp`]: content }) })
-        })),
+    const contents: string[] = await Promise.all(
+      directory.map((file: string) => readFile(join(metadataDir, file), { encoding: 'utf8' })),
     )
 
-    const categories: Record<string, string[]> = {}
+    const categories: Categories = contents
+      .reduce((prev: Categories, data: string): Categories => {
+        const { title, topics } = JSON.parse(data)
 
-    contents.forEach((data: Record<string, string>) => {
-      const [file, category] = Object.entries(data).flat()
-      const { topics } = JSON.parse(category)
+        topics.forEach((topic: string) => {
+          const prevRef = prev
+          if (!(topic in prev)) {
+            prevRef[topic] = []
+          }
 
-      topics.forEach((topic: string) => {
-        if (!(topic in categories)) {
-          categories[topic] = []
-        }
+          prevRef[topic].push(title)
+        })
 
-        categories[topic].push(file)
-      })
-    })
+        return prev
+      }, {})
 
     await writeFile(
       join(rootDir, '/assets/categories.json'),
