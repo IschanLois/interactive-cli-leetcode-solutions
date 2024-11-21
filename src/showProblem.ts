ac8bb88839969c4913d47c17650dc16b90db937a
Christian Olo <christianlois.olo@gmail.com>
2024-11-21 19:05:47 +0800

    feat: add os awareness for the preview command

--- src/showProblem.ts
+++ src/showProblem.ts
@@ -1,13 +1,29 @@
 import { execSync } from 'child_process'
+import { readFileSync } from 'fs'
 
 import buildAssetsPath from './utils/buildAssetsPath.js'
+import { Platform } from './types.js'
 
 const showProblem = (problem: string) => {
   const codePath: string = buildAssetsPath(
     `code/${problem.toLowerCase().replaceAll(' ', '-')}.cpp`,
   )
 
-  execSync(`less ${codePath}`, { stdio: 'inherit' })
+  try {
+    switch (process.platform) {
+      case Platform.Windows:
+        execSync(`more ${codePath}`, { stdio: 'inherit' })
+        break
+      case Platform.Mac:
+      case Platform.Linux:
+        execSync(`less ${codePath}`, { stdio: 'inherit' })
+        break
+      default:
+        console.log(readFileSync(codePath, 'utf8'))
+    }
+  } catch (error) {
+    console.log('There was an error with showing the problem', error)
+  }
 }
 
 export default showProblem
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,7 @@
 export type Problems = Record<string, string[]>
+
+export enum Platform {
+  Windows = 'win32',
+  Mac = 'darwin',
+  Linux = 'linux',
+}
