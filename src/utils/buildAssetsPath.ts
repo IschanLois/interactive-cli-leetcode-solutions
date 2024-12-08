62ce36db32e743d688f7780b71d570eb4bb047a1
Christian Olo <christianlois.olo@gmail.com>
2024-12-08 21:12:10 +0800

    fix: fix showing top level

--- src/utils/buildAssetsPath.ts
+++ src/utils/buildAssetsPath.ts
@@ -1,8 +1,7 @@
-import { execSync } from 'child_process'
-import { join } from 'path'
+import { join, normalize } from 'path'
 
 export default (target: string): string => join(
-  execSync('git rev-parse --show-toplevel', { encoding: 'utf8' }).trim(),
+  normalize(join(__dirname, '../../../')),
   'assets',
   target,
 )
